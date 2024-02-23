2dad329605b093ff9f12cf8af6e9be5c23b298ab
SF4NU <mariusmarius200121@gmail.com>
2024-02-23 20:05:59 +0100

    piccolo cambiamento

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: /react-gioco-di-carte-con-api/,
+  base: "/react-gioco-di-carte-con-api/",
   plugins: [react()],
 })
